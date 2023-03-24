056135f89f89dc1e033170320c5ebd5cc6899b7f
dortlii <22558338+dortlii@users.noreply.github.com>
2023-03-24 12:00:49 +0100

    test: adding test with configurable sleeping time

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type SpySleeper struct {
@@ -30,6 +31,14 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func TestCountdown(t *testing.T) {
@@ -73,4 +82,16 @@ Go!`
 		}
 	})
 
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
